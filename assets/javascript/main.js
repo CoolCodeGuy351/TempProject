cdf018254d332513f96c5401c3019ae29af7b88c
Joe Mulick <joemulick@gmail.com>
2017-04-28 00:34:29 -0700

    recent js updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,32 +1,75 @@
-/* Javascript - Jquery */ /* API's OMDB - YOUTUBE - The Movie DB */
+    /* Javascript | Jquery */
 
-/* Create Additional Playlist */
-
-var initialPlayList = [];
+    // Need to give all genre's attribute of = "genre-name"
+/// The plan is to store the initial *unfiltered* movie list from The Movie DB in the initialPlaylist array 
+var initialPlaylist = [];
+/// Then we need to filter initialPlaylist into filteredPlaylistArrayt
 var filteredPlaylist = [];
+var userProfile = [];
 
-    $('#CREATE-NEW-PLAYLIST-BUTTON').on("click", function(){
-            createNewPlaylist()
-        })
+    // FRONT END TO DO - In order for this onclick to work each dropdown item needs to have the class "genre" and a value="genreName"
+    $('.genre').on('click', function(){
+        // checks to make sure userProfile is empty then adds the value of the drop down genre selected to the empty userProfile array
+        if(userProfile.length === 0){
+            userProfile.unshift($(this).val());
+            questionStartFromSecond()
+        }
+
+    });
+
+
+    function questionsEngage(){
+        // NEED TO CHANGE TO PROPER DIV ID
+        $('#QUESTION-DIV').empty();
+
+        if(userProfile.length === 0){
+
+            var genreChoicesArray = ["Action","Adventure","Animation","Comedy","Crime","Documentary","Drama","Family","Fantasy","History","Horror","Music","Mystery","Romance","Science Fiction","TV Movie","Thriller","War","Western"];
+            var questionOneDiv = $('<div>').addClass('question-one-div');
+
+            var questionOneText = $('<p>').addClass('question-one-text').text('Choose a genre');
+            var questionOneButtonContainer = $('<div>').addClass('question-one-button-div');
+
+            for(var i = 0; i < genreChoicesArray.length ; i++){
+                var button = $('<button>').addClass('genre-buttons-question-one').text(genreChoicesArray[i]).attr('value', genreChoicesArray[i]);
+                questionOneButtonContainer.append(button)
+            }
+
+        }
+
+        var questionTwoDiv = $('<div>').addClass('question-two-div');
+        // NEED TO GET TO GATHER WITH TEAM TO DISCUSS HOW WE WANT TO CHOOSE THE YEAR RANGE - INPUT BOXES ? RADIO BUTTONS ? SOME TYPE OF ADDON THAT ALLOWS US TO CHOOSE IN A RANGE ALL FANCY
+        var questionTwoText =$('<p>').addClass('question-two-html').text("Pick A Year Range: OPTION 1 : OPTION 2");
+        questionTwoDiv.append(questionTwoHTML);
+
+
+    }
+
 
 
     // This butoon creates a new playlist if the user clicks on the create new playlist button OR is new to the site
     function createNewPlaylist(){
 
-        var newButton = $('<button>');
-        newButton
+        var genreChoicesArray = ["Action","Adventure","Animation","Comedy","Crime","Documentary","Drama","Family","Fantasy","History","Horror","Music","Mystery","Romance","Science Fiction","TV Movie","Thriller","War","Western"];
+        var questionOneDiv = $('<div>').addClass('question-one-div');
+
+        var questionOneText = $('<p>').addClass('question-one-text').text('Choose a genre');
+        var questionOneButtonContainer = $('<div>').addClass('question-one-button-div');
+
+        // This will Add buttons into questionOneButtonContainer that can be styles accordingly
+        for(var i = 0; i < genreChoicesArray.length ; i++){
+                var button = $('<button>').addClass('genre-buttons-question-one').text(genreChoicesArray[i]).attr('value', genreChoicesArray[i]);
+                questionOneButtonContainer.append(button)
+        }
         
     }
 
 
-// This line renders and previously created playlists to the screen
-    function renderPlaylist() {
+// This line renders any previously created playlists to the screen
+    function renderPlaylist(){
 
-        // Deleting the gifs prior to adding new gifs
         $("#playlist-container").empty();
-
         for (var i = 0; i < playList.length; i++) {
-
           var buttonGen = $("<button>");
           // Adding a class of gif to our button
           buttonGen.addClass("playlist");
@@ -38,94 +81,10 @@ var filteredPlaylist = [];
           $("#PLAYLIST-CONTAINER").append(buttonGen);
 
         }
-      } // RenderPlaylist function end
+      }
 
-
-    // When the user clicks on an already created playlist we switch cotents in the Data Section / The Main Video-player section / and the Purchase options section
-    function changePlaylist(){
-        $('DATA-SECTION').html()
-        $('MAIN-VIDEO-SECTION').html()
-        $('PURCHASE-SECTION').html()
-    }
-
-    function renderNewContent() {
-
-        $('#div-main-section').empty();
-
-        var gifName = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifName + "&limit=10&api_key=dc6zaTOxFJmzC";
-
-        // Creating an AJAX call for the specific gif button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-
-          for(var i = 0 ; i < 10 ; i++){
-
-          // Creating a div to hold the gif
-          var gifDiv = $("<div class='gifBlock'>").css('display','inline-block');
-          // Grab the rating for the button clicked and store it in a variable
-
-          var rating = response.data[i].rating;
-          var gifImageURL = response.data[i].images.fixed_height_still.url;
-          var gifGifURL = response.data[i].images.fixed_height.url;
-
-          var gifImage = $('<img>').attr("src", gifImageURL).attr("data-animate", gifGifURL).attr("data-still", gifImageURL).attr("data-state", gifState).css('display','block').addClass('gif');
-          var ratingContent = $("<p>Rating: " + rating + "</p>");
-
-          gifDiv.append(gifImage);
-          gifDiv.append(rating);
-
-          // Putting the entire gif above the previous gifs
-          $("#div-main-section").append(gifDiv);
-
-        }
-
-        });
-
-}
-
-
-
-// This line will look for any playlists added and will change the playlist when clicked
-$(document).on("click", ".individualPLaylist", changePlaylist)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-
-    function initialize() {
-        var mapProp = {
-            center: myCenter,
-            zoom: 12,
-            scrollwheel: false,
-            draggable: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-        var marker = new google.maps.Marker({
-            position: myCenter,
-        });
-
-        marker.setMap(map);
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
+    // This line will look for any playlists added and will change the playlist when clicked
+    $(document).on("click", ".individualPlaylist", changePlaylist)  
 
     /* Smooth scrolling */
     $(document).ready(function() {
@@ -153,3 +112,25 @@ $(document).on("click", ".individualPLaylist", changePlaylist)
             } // End if
         });
     });
+
+        var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+
+    function initialize() {
+        var mapProp = {
+            center: myCenter,
+            zoom: 12,
+            scrollwheel: false,
+            draggable: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        var marker = new google.maps.Marker({
+            position: myCenter,
+        });
+
+        marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
